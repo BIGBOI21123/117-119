ba829a0e2d88ee14e22a716f2d73465446a7d182
Anish Renukunta <75855908+BIGBOI21123@users.noreply.github.com>
2022-04-09 12:56:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup() {
+    Canvas = createCanvas(300, 300);
+    Canvas.center();
+    background("white");
+}
+
+function draw() {
+    
+}
+
+function Clearcanvas() {
+    background("white");
+}
